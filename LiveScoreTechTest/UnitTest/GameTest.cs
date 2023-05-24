c419f724e285b2cdfa295feab166debbd60d5733
David Amian <david.amian@travelgate.com>
2023-05-24 12:05:57 +0200

    Rename

--- LiveScoreTechTest/UnitTest/GameTest.cs
+++ LiveScoreTechTest/UnitTest/GameTest.cs
@@ -2,15 +2,20 @@ using LiveScoreLib.Domain;
 
 namespace UnitTest;
 
-public class UnitTest1
+public class GameTest
 {
     [Theory]
     [InlineData("team1", "team2",true, 0,1)]
     [InlineData("team2", "team1",false, 2,3)]
     [InlineData("team4", "team3",true, 1,4)]
-    public void GameCreatedOk(string homeTeam, string awayTeam, bool finished, int homeGoals, int awayGoals)
+    public void Game_Created_Ok(string homeTeam, string awayTeam, bool finished, int homeGoals, int awayGoals)
     {
+        //Arrange
+        
+        //Act
         var game = new Game(homeTeam, awayTeam, finished, homeGoals, awayGoals);
+        
+        //Assert
         Assert.Equal(game.HomeTeam, homeTeam);
         Assert.Equal(game.AwayTeam, awayTeam);
         Assert.Equal(game.IsFinish(), finished);
@@ -21,10 +26,13 @@ public class UnitTest1
     [Theory]
     [InlineData(TeamType.Away)]
     [InlineData(TeamType.Home)]
-    public void GameIncreaseScore(TeamType team)
+    public void Game_Increase_Score(TeamType team)
     {
+        //Arrange
         var game = new Game("team1", "team2");
+        //Act
         game.IncreaseScore(team);
+        //Assert
         Assert.Equal(1, team == TeamType.Away ? game.AwayScore : game.HomeScore);
     }
 }
